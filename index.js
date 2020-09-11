3abc148e51db7cf2f8846f46ac62814bb84f4470
natalie_ng <natalie.ng.14755@gmail.com>
2020-09-10 23:01:06 -0400

    chore: removed extra comment

--- index.js
+++ index.js
@@ -4,11 +4,6 @@ const bodyParser = require("body-parser");
 const app = express();
 
 const config = require("./server/config/keys");
-// const mongoose = require("mongoose");
-// mongoose.connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-//   .then(() => console.log('MongoDB Connected...'))
-//   .catch(err => console.log(err));
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
